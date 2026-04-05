47fcbb32cfe91c4aff385aeb64526040b16e3eaf
nullpointers0402 <nullpointers492@gmail.com>
2026-04-05 12:05:33 +0530

    Add files via upload
    Added Calculator -Varun

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+a =int (input("Enter a number: " ))
+b =int(input("Enter another number: "))
+print("Sum :", a + b)
+print("Difference :", a - b)
+print("Product :", a * b)
+print("Division:", a / b)
+print("Exponentation :", a ** b)
